59cf323cd4d478085444de7bdcc7d136a0cd95c5
Raul <rauljeverts@gmail.com>
2026-07-02 23:20:21 +0200

    Added NotificationRepository and Jpa queries

--- src/main/java/za/ac/cput/repository/NotificationRepository.java
+++ src/main/java/za/ac/cput/repository/NotificationRepository.java
@@ -1,4 +1,20 @@
 package za.ac.cput.repository;
 
-public class NotificationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Notification;
+import za.ac.cput.domain.enums.NotificationStatus;
+import za.ac.cput.domain.enums.NotificationType;
+import za.ac.cput.domain.user.Patient;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+List<Notification> findByPatient(Patient patient);
+
+List<Notification> findByNotificationStatus(NotificationStatus notificationStatus);
+
+List<Notification> findByNotificationType(NotificationType notificationtype);
+
+
 }
